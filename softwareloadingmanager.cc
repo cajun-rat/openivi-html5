72ed87840018e728442267e224718f13b5dad603
Phil Wise <phil@advancedtelematic.com>
2015-11-03 15:49:03 +0100

    Interface to Genivi Software Loading Manager
    Provide a genivi.slm interface to the Software Loading Manager DBUS API.

--- softwareloadingmanager.cc
+++ softwareloadingmanager.cc
@@ -1,3 +1,26 @@
 #include "softwareloadingmanager.h"
 
+using org::genivi::software_loading_manager;
 
+SoftwareLoadingManager::SoftwareLoadingManager(QObject *parent)
+    : QObject(parent),
+      slm_(new software_loading_manager("org.genivi.software_loading_manager",
+                                        "/org/genivi/software_loading_manager",
+                                        QDBusConnection::sessionBus(), this))
+
+{
+  connect(slm_, SIGNAL(update_state_changed(int, int)), this,
+          SIGNAL(update_state_changed(int, int)));
+}
+
+SoftwareLoadingManager::~SoftwareLoadingManager() {}
+
+int SoftwareLoadingManager::update_count() {
+  return slm_->update_count().value();
+}
+
+int SoftwareLoadingManager::update_state() {
+  return slm_->update_state().value();
+}
+
+void SoftwareLoadingManager::approve() { slm_->approve().reply(); }
